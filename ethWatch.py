3fce93c55c721761adda0ec04bcb2268d562d98b
DrakeEvans <ecg.drake@gmail.com>
2017-12-08 08:42:31 -0800

    update

--- ethWatch.py
+++ ethWatch.py
@@ -10,6 +10,8 @@ import ctypes
 
 #Config Values
 GPUIndex = sys.argv[1]
+powerLevel = sys.argv[3]
+memoryOffset = sys.argv[2]
         
 workingDir = "C:\\Users\Miner\ethWatchdog"
 
@@ -25,89 +27,108 @@ def readLastLine(filePath, mode):
     lastLine = outputLines[len(outputLines)-1]
     return lastLine
 
-restartCounter = 0
-while (restartCounter <15):
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
-    ethminerLogName = f'{workingDir}\ethminerOutput{GPUIndex}.log'
+if is_admin(): 
 
-    #Start Main Subprocess and write output to file
-    arg = shlex.split(f'C:\program files\ethminer\ethminer.exe --farm-recheck 1000 -U --cuda-devices {GPUIndex} -M -S us1.ethermine.org:4444 -FS us2.ethermine.org:4444 -O 0x742a902f4a6c6aa1f59a4ef0b7d72fec6717f207.MINER', posix=0)
-    print(arg)
-    ethminer = subprocess.Popen(arg, stderr=subprocess.STDOUT, stdout=open(ethminerLogName, 'a'))
-    print('Subprocess Started')
+    restartCounter = 0
+    while (restartCounter <100):
 
+        ethminerLogName = f'{workingDir}\ethminerOutput{GPUIndex}.log'
 
-    #Sleep while ethminer subprocess begins, impossible to access file before it is created
-    time.sleep(20)
-
-
-    #Monitor for Two Hours
-    previousLine = "nothing"
-    lastSpeed = 'nothing'
-    i = 0
-    while True:
-        i = i + 1
-        #Get Last Line of the output file
-        lastLine = readLastLine(ethminerLogName, 'r')
-        print(lastLine, end="")
+        #Start Main Subprocess and write output to file
+        argString = shlex.split(f'C:\\Users\Miner\Downloads\Guru3D.com\\nvidiaInspector.exe -setMemoryClockOffset:{GPUIndex},0,{memoryOffset}', posix=0)
+        nvidiaPower = subprocess.Popen(argString)
+    
         
-        #Get the current Time
-        timeRegex = re.compile(r'\S+(?=\|)')
-        matchTime = timeRegex.search(lastLine)
-        currentTime = 'NA'
-        if matchTime:
-            currentTime = matchTime.group(0)
+        nvidiaInspector = executeSubprocess(f'nvidia-smi -pl {powerLevel} -i {GPUIndex}')
         
-        #Get the current Speed
-        speedRegex = re.compile(r'\S+(?=\sMh/s)')
-        matchSpeed = speedRegex.search(lastLine)
-        currentSpeed = 'NA'
-        if matchSpeed:
-            currentSpeed = matchSpeed.group(0)
-        
-        #Get the current clock, and power settings
-        nvidiaSMI = executeSubprocess(f'nvidia-smi -i {GPUIndex} -q')
-        time.sleep(5)
-        output = nvidiaSMI.communicate()
-        
-        # Get Power Readings_
-        powerRegex = re.compile(r'\S*(?=\sW\\r\\n\s+Power Limit)')
-        currentPower = 'No power output match'
-        matchPower = powerRegex.search(str(output[0]))
-        if matchPower:
-            currentPower = matchPower.group(0)
-
-        # Get Clock Readings
-        clockRegex = re.compile(r'\S+(?=\sMHz\\r\\n\s+SM)')
-        currentClock = 'No clock output match'
-        matchClock = clockRegex.search(str(output[0]))
-        if matchClock:
-            currentClock = matchClock.group(0)
-
-        # Get Memory Readings
-        memoryRegex = re.compile(r'\S+(?=\sMHz\\r\\n\s+Video)')
-        currentMemory = 'No memory output match'
-        matchMemory = memoryRegex.search(str(output[0]))
-        if matchMemory:
-            currentMemory = matchMemory.group(0)
-        
-        if i % 5 == 0:
-            print(f'\nCurrent Speed: {currentSpeed}', file=open(ethminerLogName + '_', 'a'))
-            print(f'lastSpeed: {lastSpeed}')
-            print(f'Last Line: {lastLine}', end='')
-            print(f'previousLine: {previousLine}', end='')
-            if lastLine == previousLine or (currentSpeed == 'NA' and lastSpeed == 'NA'):
-                print('Error restarting subprocess')
-                break
-            else:
-                previousLine = lastLine
-                lastSpeed = currentSpeed
+        arg = shlex.split(f'C:\program files\ethminer\ethminer.exe --farm-recheck 1000 -U --cuda-devices {GPUIndex} -M -S us1.ethermine.org:4444 -FS us2.ethermine.org:4444 -O 0x742a902f4a6c6aa1f59a4ef0b7d72fec6717f207.MINER', posix=0)
+        print(arg)
+        ethminer = subprocess.Popen(arg, stderr=subprocess.STDOUT, stdout=open(ethminerLogName, 'a'))
+        print('Subprocess Started')
 
 
-        print(f'Clock: {currentClock}, Memory: {currentMemory}, Power: {currentPower}, Restarts: {restartCounter}\n')
+        #Sleep while ethminer subprocess begins, impossible to access file before it is created
+        time.sleep(15)
 
 
-    ethminer.terminate()
-    time.sleep(1)
-    restartCounter += 1
+        #Monitor for Two Hours
+        previousLine = "nothing"
+        lastSpeed = 'nothing'
+        i = 0
+        while True:
+            i = i + 1
+            #Get Last Line of the output file
+            lastLine = readLastLine(ethminerLogName, 'r')
+            print(lastLine, end="")
+            
+            #Get the current Time
+            timeRegex = re.compile(r'\S+(?=\|)')
+            matchTime = timeRegex.search(lastLine)
+            currentTime = 'NA'
+            if matchTime:
+                currentTime = matchTime.group(0)
+            
+            #Get the current Speed
+            speedRegex = re.compile(r'\S+(?=\sMh/s)')
+            matchSpeed = speedRegex.search(lastLine)
+            currentSpeed = 'NA'
+            if matchSpeed:
+                currentSpeed = matchSpeed.group(0)
+            
+            #Get the current clock, and power settings
+            nvidiaSMI = executeSubprocess(f'nvidia-smi -i {GPUIndex} -q')
+            time.sleep(5)
+            output = nvidiaSMI.communicate()
+            nvidiaSMI.terminate()
+            time.sleep(10)
+            
+            # Get Power Readings_
+            powerRegex = re.compile(r'\S*(?=\sW\\r\\n\s+Power Limit)')
+            currentPower = 'No power output match'
+            matchPower = powerRegex.search(str(output[0]))
+            if matchPower:
+                currentPower = matchPower.group(0)
 
+            # Get Clock Readings
+            clockRegex = re.compile(r'\S+(?=\sMHz\\r\\n\s+SM)')
+            currentClock = 'No clock output match'
+            matchClock = clockRegex.search(str(output[0]))
+            if matchClock:
+                currentClock = matchClock.group(0)
+
+            # Get Memory Readings
+            memoryRegex = re.compile(r'\S+(?=\sMHz\\r\\n\s+Video)')
+            currentMemory = 'No memory output match'
+            matchMemory = memoryRegex.search(str(output[0]))
+            if matchMemory:
+                currentMemory = matchMemory.group(0)
+            
+            if i % 1 == 0:
+                print(f'\nCurrent Speed: {currentSpeed}', file=open(ethminerLogName + '_', 'a'))
+                print(f'lastSpeed: {lastSpeed}')
+                print(f'Last Line: {lastLine}', end='')
+                print(f'previousLine: {previousLine}', end='')
+                if lastLine == previousLine or (currentSpeed == 'NA' and lastSpeed == 'NA'):
+                    print('Error restarting subprocess')
+                    break
+                else:
+                    previousLine = lastLine
+                    lastSpeed = currentSpeed
+
+
+            print(f'Clock: {currentClock}, Memory: {currentMemory}, Power: {currentPower}, Restarts: {restartCounter}\n')
+
+
+        ethminer.terminate()
+        time.sleep(1)
+        restartCounter += 1
+
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f'C:\\Users\Miner\ethWatchdog\ethWatch.py {GPUIndex} {memoryOffset} {powerLevel}', None, 1)
